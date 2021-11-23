0183dc59ffa946f7a6c12ca61654010897c20eea
DXTkastb <dxtkastb@gmail.com>
2021-11-23 18:38:50 +0530

    code format updation

--- src/main/java/com/app/regexcalc/RegexCalculator.java
+++ src/main/java/com/app/regexcalc/RegexCalculator.java
@@ -11,60 +11,50 @@ import com.app.results.RegResult;
 
 @Service
 public class RegexCalculator {
-	
-	
-	
 
-	public RegResult result(String rSTR,String str) {
+	public RegResult result(String rSTR, String str) {
+
+		RegResult rr = new RegResult();
+		rr.setMatch(false);
+		rr.setMatchingGroups(null);
 		
-		RegResult rr=new RegResult();
-		Pattern p=patternGetter(rSTR);
-		
-		Matcher m=matcher(p,str);
-		boolean isMatch=m.matches();
-		if(isMatch==true) {
-		rr.setMatch(isMatch);
-		rr.setMatchingGroups(regexGroupMatcher(m));}
-		else {
-			
-			rr.setMatch(false);
-			rr.setMatchingGroups(null);
+		Pattern p = patternGetter(rSTR);
+		Matcher m = matcher(p, str);
+
+		boolean isMatch = m.matches();
+		if (isMatch == true) {
+			rr.setMatch(isMatch);
+			rr.setMatchingGroups(regexGroupMatcher(m));
 		}
-		
+
 		return rr;
 	}
-	
-	
+
 	public Pattern patternGetter(String regex_String) {
-		
+
 		return Pattern.compile(regex_String);
-		
+
 	}
-	
-	public Matcher matcher(Pattern pattern,String string) {
-		
+
+	public Matcher matcher(Pattern pattern, String string) {
+
 		return pattern.matcher(string);
-		
+
 	}
-	
-	
-	
-	public List<String> regexGroupMatcher(Matcher m){
-		
-		ArrayList<String> matchingStringGroups=new ArrayList();
-		
-		int groups=m.groupCount();
-		
-		while(groups>-1) {
-			matchingStringGroups.add("group "+groups+" : "+m.group(groups));
+
+	public List<String> regexGroupMatcher(Matcher m) {
+
+		ArrayList<String> matchingStringGroups = new ArrayList();
+
+		int groups = m.groupCount();
+
+		while (groups > -1) {
+			matchingStringGroups.add("group " + groups + " : " + m.group(groups));
 			--groups;
 		}
-		
+
 		return matchingStringGroups;
-		
-		
-		
+
 	}
-	
 
 }
